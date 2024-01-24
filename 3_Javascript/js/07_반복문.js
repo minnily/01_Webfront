fd253a1a3345a7d238b9afbbd49e156dc5c0a485
minnily <156267800+minnily@users.noreply.github.com>
2024-01-24 17:30:23 +0900

    2024.01.24

--- 3_Javascript/js/07_반복문.js
+++ 3_Javascript/js/07_반복문.js
@@ -98,3 +98,133 @@ function executeFn2() {
     //(HTML) 태그를 해석하지 않고 문자열 그대로 보여줌
     //요소.innerHTML = `<li>${num}</li>`; 
     //-> HTML 태그를 해석해서 화면에 태그의 본 기능대로 출력
+
+
+function executeFn3(){
+    const input  = Number(document.getElementById("input3").value);// 입력된 값(단)
+    const ul = document.getElementById("result3"); //결과를 출력할 요소 ul태그
+    ul.innerHTML = "";// 이전내용 삭제
+
+    //입력받은 단이 2~9 사이가 아닐 경우 "2~9사이만 입력해주세요" 알림창 띄우기
+    if(input <2 || input >9){
+        alert("2~9사이만 입력해 주세요");
+        return; //return 뒤에 값을 쓰면 함수를 종료하고 호출한 곳으로 돌아가는 역할인데
+                //현재는 값을 뒤에 쓰지 않고 함수 종료 정도로만 인식하는 역할을 함
+    }           
+
+    for(let num = 1; num <= 9; num++){
+    // num이 1부터 인 이유는 구구단의 시작이 0*1~0*9로 시작하기 때문
+    //단은 input 곱해지는 값 num으로 작성할 예정 += :누적시키는것
+    ul.innerHTML += `<li>${input} x ${num} = ${input * num} </li>`;
+
+    }
+}    
+/*다음 모양 출력하기
+12345
+12345
+12345
+12345 
+*/
+
+function check8(){
+    
+    //4바퀴 반복하는 for문
+    for(let y= 1; y <= 4; y++){
+
+        //"12345"를 출력하는 구문 만들기
+        let str = ""; //결과 저장용 만들기
+        for(let x =1 ; x <= 5; x++){
+             str += x;
+            //"12345"
+        }
+
+        console.log(str);
+    }
+}
+
+
+function check9(){
+    for(let y=1; y<=5; y++){
+        let str = "";
+        for(let x = 1; x <=y; x++){ //x가 y번째만큼 하나씩 늘어나야하기에
+            str += x;
+        }
+        console.log(str);
+    }
+}
+
+function check16(){
+    //변수 선언만 해두기
+
+    let val; //undefined
+
+    //취소를 누르기 전까지 계속 반복
+    //== 취소를 누르면 반복하지 않겠다(종료하겠다)
+
+    while(val !== null){
+        //!== : 동일 비교 연산자 (값과 자료형이 모두 다른경우 TRUE)
+        //A!=B : A와 B의 값이 다른경우
+        //=== : 값, 자료형이 모두 같은 경우 TRUE
+    
+
+        val = prompt("입력 후 확인"); // 변수에 prompt 값 대입
+        // prompt 값 확인 누르면 입력한 값 들어오고 취소 누르면 null
+
+        console.log(val);
+    }   
+}
+
+function check17(){
+    //메뉴가격
+    const gimbap =3000;
+    const ramen =3500;
+    const donkkaseu =5000; 
+    
+    //주문 개수 카운트
+
+    let gcount = 0; //김밥
+    let rcount = 0; //라면
+    let dcount = 0; //돈까스
+
+
+    //prompt로 입력한 값을 저장할 변수 선언
+    let input; //현재 상태 undefined
+    
+    //확인버튼 : 입력한 값 , 취소버튼 : null
+
+    while(input !== null) { //취소 누르기 전까지 반복
+
+        input = prompt("메뉴번호를 입력하세요!");
+
+        switch(input){
+            case "1" : gcount++; break;
+            case "2" : rcount++; break;
+            case "3" : dcount++; break;
+            case null : alert("주문완료!"); break;
+            default  : alert("메뉴에 작성된 번호만 입력해주세요"); break; 
+        }
+    }
+
+    alert(`김밥:${gcount},라면:${rcount},돈까스:${dcount}`);
+    let sum =(gcount * gimbap) + (rcount * ramen) + (dcount * donkkaseu);
+    alert(`총가격: ${sum}원`);
+}
+
+
+function check18(){
+    //1부터 10까지 출력
+    let num = 1; 
+    while(num < 11){
+        console.log(num);
+
+        num++;; //이걸 쓰지않으면 브라우저 멈출수 있음!
+    }
+    console.log('---')
+    //10부터 1까지 1씩 감소
+
+    let x= 10;
+    while(x > 0){
+        console.log(x);
+        x--;;
+    }
+}
---
+++ 3_Javascript/js/08_분기문.js
@@ -0,0 +1,68 @@
+function check1(){
+    //1부터 10까지 1씩 증가하다가 5가 되면 멈춤
+    for(let i =1; i <=10; i++){
+        console.log("i", i);
+
+        if(i == 5) break; //한줄만 있는 코드면 {}안써도됨
+    }
+
+}
+
+//무한 반복하는 while문 멈추기
+function check2(){
+    let i =1;
+    //i가 10초과시 멈추게 할예정
+    while(true){
+        //true일때 반복수행할 코드 작성
+
+        console.log("i",i++);
+
+        if(i > 10) break;
+    }
+}
+
+// countinue 확인하기
+function check3(){
+    //1부터 20까지 출력. 단 3의 배수는 건너뛰기
+    for(let i = 1; i<= 20; i++){
+       
+        //3의 배수인 경우
+        
+        if(i % 3 == 0) continue;
+    // 3의 배수인 경우 아래코드는 수행하지 않음.
+        console.log("i:", i);
+    }
+}
+//1부터 30까지 1씩 증가하며 출력 
+//단, 홀수 또는 10의 배수는 건너뛰기
+function check4(){
+    for(let i=1; i <=30; i++){
+        if(i % 2 == 1 ||i % 10 == 0 ) continue;
+    
+        console.log("i:", i);
+    }
+    
+}
+ 
+// 0~9까지 5줄 출력
+//-각 줄에서 4의 배수는 건너뛰기
+//-3번째 줄 출력 후 멈추기
+function check5(){
+    for(let row = 1; row<=5; row++);{
+        let str = "";
+        for(let col = 0; col <=9; col++){
+            str += col;
+        //을 제외한 4의 배수
+        if(col !=0 && col % 4 == 0) continue;
+
+        str += col;
+    
+        }
+        console.log(str);
+        //3번째 줄 출력 후 멈춤
+        if(row == 3) break;
+    }
+
+
+
+}
